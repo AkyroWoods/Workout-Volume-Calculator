9ddac421b04fc7333e6f7ef40b41c6b4e2446c67
Akyro Woods <akyrowoods07@gmail.com>
2026-01-20 19:56:30 -0700

    Improve input validation and enforce Exercise constructor rules

--- src/Exercise.java
+++ src/Exercise.java
@@ -4,7 +4,19 @@ public class Exercise {
     private int reps;
     private double weight;
 
-    public Exercise(String name, int sets, int reps, double weight) {
+    public Exercise(String name, int sets, int reps, double weight)  throws IllegalArgumentException{
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("Exercise name cannot be blank");
+        } 
+        if (sets < 1) {
+            throw new IllegalArgumentException("Sets cannot be less than 1");
+        }
+        if (reps < 1) {
+            throw new IllegalArgumentException("Reps cannot be less than 1");
+        }
+        if (weight < 0) {
+            throw new IllegalArgumentException("Weight cannot be negative");
+        }
         this.name = name;
         this.sets = sets;
         this.reps = reps;
--- src/UserInterface.java
+++ src/UserInterface.java
@@ -11,29 +11,38 @@ public class UserInterface {
         System.out.print("Name of Workout: ");
         String workoutName = scanner.nextLine();
 
+        while (workoutName.isBlank()) {
+            System.out.println("\u001B[31m" + "Invalid workout name" + "\u001B[0m");
+            System.out.print("Name of Workout: ");
+            workoutName = scanner.nextLine();
+        }
+
         Workout workout = new Workout(workoutName);
 
-    
+        System.out.println();
+        commandList();
 
         while (true) {
-            System.out.println("1: Add exercise");
-            System.out.println("2: List workout");
-            System.out.println("3: Calculate total workout volume");
-            System.out.println("4: show highest volume exercise");
-
+            System.out.println();
             System.out.print("Command: ");
-            int commandInput = Integer.valueOf(scanner.nextLine());
-
-            if (commandInput == -1) {
-                break;
-            }
+           String commandInput = scanner.nextLine();
             commandProcessor(workout, commandInput);
         }
 
     }
 
-    public void commandProcessor(Workout workout, int commandInput) {
-        if (commandInput == 1) {
+    public void commandProcessor(Workout workout, String commandInput) {
+        
+        if (commandInput.equalsIgnoreCase("help")) {
+            System.out.println();
+            commandList();
+            return;
+        } else if (commandInput.equalsIgnoreCase("quit")) {
+            System.exit(0);;
+        } else if (isInteger(commandInput)) {
+            int command = Integer.parseInt(commandInput);
+
+            if (command == 1) {
             System.out.print("Name: ");
             String name = scanner.nextLine();
             System.out.print("Sets: ");
@@ -49,15 +58,56 @@ public class UserInterface {
             System.out.println();
             System.out.println("\u001B[32m" + "Exercise added" + "\u001B[0m");
             System.out.println();
-        } else if (commandInput == 2) {
+        } else if (command == 2) {
+            if (workout.size() == 0) {
+                noExercisesAddedErrorMessage();
+                return;
+            }
             workout.printWorkout();
             System.out.println();
-        } else if (commandInput == 3) {
+
+        } else if (command == 3) {
+            if (workout.size() == 0) {
+                noExercisesAddedErrorMessage();
+                return;
+            }
             System.out.println(workout.calculateTotalWorkoutVolume());
+
             System.out.println();
-        } else if (commandInput == 4) {
+        } else if (command == 4) {
+            if (workout.size() == 0) {
+                noExercisesAddedErrorMessage();
+                return;
+            }
+
             workout.highestVolumeExercise();
             System.out.println();
+        } 
+        else 
+            System.out.println("Unknown Command");
+        } else 
+            System.out.println("Unknown Command");
+        
+        
+    }
+
+    private void commandList() {
+        System.out.println("1: Add exercise");
+        System.out.println("2: List workout");
+        System.out.println("3: Calculate total workout volume");
+        System.out.println("4: show highest volume exercise");
+        System.out.println("help - list commands again ");
+    }
+    private boolean isInteger(String input) {
+        try {
+            Integer.parseInt(input);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
         }
+    }
+    private void noExercisesAddedErrorMessage() {
+        System.out.println("\u001B[31m" + "No exercises are in the workout" + "\u001B[0m");
+
     }
 }
